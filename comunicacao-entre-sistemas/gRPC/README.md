a58c174cdc18d9dc2464655e0f2f16ab59bf0189
Rochele Edenís <rochele.edenis@gmail.com>
2023-02-26 16:53:35 -0300

    Listando categories

--- comunicacao-entre-sistemas/gRPC/README.md
+++ comunicacao-entre-sistemas/gRPC/README.md
@@ -30,3 +30,9 @@ Inicie o Evans.
 ```bash
 evans -r repl
 ```
+
+Toda vez que o arquivo course_category é alterado precisamos rodar o comando abaixo para regerar os arquivos de comunicação.
+
+```bash
+protoc --go_out=. --go-grpc_out=. proto/course_category.proto
+```
--- comunicacao-entre-sistemas/gRPC/internal/service/category.go
+++ comunicacao-entre-sistemas/gRPC/internal/service/category.go
@@ -33,3 +33,24 @@ func (c *CategoryService) CreateCategory(ctx context.Context, input *pb.CreateCa
 
 	return categoryResponse, nil
 }
+
+func (c *CategoryService) ListCategories(ctx context.Context, input *pb.Blank) (*pb.CategoryList, error) {
+	categories, err := c.CategoryDB.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var categoriesResponse []*pb.Category
+
+	for _, category := range categories {
+		categoryResponse := &pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		}
+
+		categoriesResponse = append(categoriesResponse, categoryResponse)
+	}
+
+	return &pb.CategoryList{Categories: categoriesResponse}, nil
+}
